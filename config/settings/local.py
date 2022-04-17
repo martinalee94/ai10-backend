1ca12b489776649cf33dbb720cc7992dd38b7472
Martina Lee <martinalee0528@gmail.com>
2022-04-17 14:24:50 +0900

    chore: dotenv 추가
    - .env.prod가 아니라 .env로 파일명설정해야 환경변수 로딩 가능함

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,4 +3,4 @@ import os
 from .base import *  # noqa: F403, F401
 
 DEBUG = True
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split()
